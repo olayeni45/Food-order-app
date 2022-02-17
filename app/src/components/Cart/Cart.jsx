d6ff15c90fe9966dc00daf8e109e3657dd614cbc
Olayeni Anifowose <olayeni45@gmail.com>
2022-02-17 15:22:59 +0000

    added cart item

--- app/src/components/Cart/Cart.jsx
+++ app/src/components/Cart/Cart.jsx
@@ -1,29 +1,46 @@
-import React from "react";
+import React, { useContext } from "react";
 import Modal from "../UI/Modal";
 import styles from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
+import CartItem from "./CartItem";
 
 const Cart = (props) => {
   const { onCloseCart } = props;
 
-  const cartItems = [{ id: "c1", name: "Sushi", amount: 2, price: 12.99 }];
+  const cartCtx = useContext(CartContext);
+
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0;
+
+  const cartItemAddHandler = (item) => {};
+
+  const cartItemRemoveHandler = (id) => {};
+
   return (
     <Modal onCloseCart={onCloseCart}>
       <ul className={styles["cart-items"]}>
-        {cartItems.map((cart) => (
-          <li>{cart.name}</li>
+        {cartCtx.items.map((cart) => (
+          <CartItem
+            key={cart.id}
+            name={cart.name}
+            amount={cart.amount}
+            price={cart.price}
+            onAdd={cartItemAddHandler.bind(null, cart)}
+            onRemove={cartItemRemoveHandler.bind(null, cart.id)}
+          />
         ))}
       </ul>
 
       <div className={styles.total}>
         <span>Total amount</span>
-        <span>35.62</span>
+        <span>{totalAmount}</span>
       </div>
 
       <div className={styles.actions}>
         <button className={styles["button--alt"]} onClick={onCloseCart}>
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
---
+++ app/src/components/Cart/CartItem.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import styles from "./CartItem.module.css";
+
+const CartItem = (props) => {
+  const { price, name, amount, onAdd, onRemove } = props;
+  const fixedPrice = `$${price.toFixed(2)}`;
+
+  return (
+    <li className={styles["cart-item"]}>
+      <div>
+        <h2>{name}</h2>
+        <div className={styles.summary}>
+          <span className={styles.price}>{fixedPrice}</span>
+          <span className={styles.amount}>x {amount}</span>
+        </div>
+      </div>
+      <div className={styles.actions}>
+        <button onClick={onRemove}>−</button>
+        <button onClick={onAdd}>+</button>
+      </div>
+    </li>
+  );
+};
+
+export default CartItem;
