efb5b3cd7efceb5806e36a8fcacacf2205766fa2
Olayeni Anifowose <olayeni45@gmail.com>
2022-02-17 16:15:15 +0000

    added button animation

--- app/src/components/Layout/HeaderCartButton.jsx
+++ app/src/components/Layout/HeaderCartButton.jsx
@@ -1,10 +1,12 @@
-import React, { useContext } from "react";
+import React, { useContext, useState, useEffect } from "react";
 import styles from "./HeaderCartButton.module.css";
 import CartIcon from "../Cart/CartIcon";
 import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
   const { onClick } = props;
+
+  const [btnIsAnimated, setBtnIsAnimated] = useState(false);
 
   const cartCtx = useContext(CartContext);
   const { items } = cartCtx;
@@ -13,8 +15,25 @@ const HeaderCartButton = (props) => {
     return currentNumber + item.amount;
   }, 0);
 
+  const buttonClasses = `${styles.button} ${btnIsAnimated ? styles.bump : ""}`;
+
+  useEffect(() => {
+    if (items.length == 0) {
+      return;
+    }
+    setBtnIsAnimated(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsAnimated(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [items]);
+
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={buttonClasses} onClick={onClick}>
       <span className={styles.icon}>
         <CartIcon />
       </span>
